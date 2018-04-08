da9b854960b198ab46b6f7cc9a037672638d81ad
David Ryan <djrdev@icloud.com>
2018-04-08 12:16:05 -0600

    add text input

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import React, { Component } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Platform ,
+  KeyboardAvoidingView,
+  TextInput
+} from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
 
   render() {
-    const { largeText, smallText, textStyle } = styles;
+    const { largeText, smallText, textStyle, textInput } = styles;
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
         <Text style={[largeText, textStyle]}>San Francisco</Text>
         <Text style={[smallText, textStyle]}>Light Cloud</Text>
         <Text style={[smallText, textStyle]}>24˚</Text>
-      </View>
+        <TextInput
+          autoCorrect={false}
+          placeholder="Search any city"
+          placeholderTextColor="white"
+          style={textInput}
+          clearButtonMode="always"   
+        />
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -31,5 +45,15 @@ const styles = StyleSheet.create({
 	},
 	smallText: {
 		fontSize: 18,
-	},
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  }
 });
